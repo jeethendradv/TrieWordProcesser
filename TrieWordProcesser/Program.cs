494c37a661fc2f219d4669151bee22b8904202e7
Jeethendra DV <jeethendradv@live.com>
2018-02-26 18:59:51 +1300

    Word search using Trie algorithm.

---
+++ TrieWordProcesser/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace TrieWordProcesser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\wordlist.txt");
+            Trie trie = new Trie();
+            List<Word> words = ReadFile(path);
+            Console.WriteLine("Number of words to process: {0}", words.Count);
+            foreach(Word word in words)
+            {
+                trie.Insert(word.Name);
+            }
+            foreach (Word word in words)
+            {
+                word.Search(trie);
+                Console.Write("\r{0}                        ", word.Name);
+            }
+            Console.WriteLine("\r              ");
+            List<Word> completewords = words.Where(x => x.IsComplete()).ToList();
+            List<Word> topTwenty = completewords
+                .OrderByDescending(x => x.NumberOfSubWords)
+                .ThenByDescending(x => x.Name.Length)
+                .Take(20)
+                .ToList();
+
+            Console.WriteLine(string.Format("First longest word: {0}", topTwenty[0].Name));
+            Console.WriteLine(string.Format("Second longest word: {0}", topTwenty[1].Name));
+            Console.WriteLine(string.Format("Total number of words constructed of other words: {0}", completewords.Count));
+            Console.WriteLine();
+            Console.WriteLine("Top 20 longest words:");
+            
+            foreach (Word w in topTwenty)
+            {
+                Console.WriteLine(string.Format("{0} = {1}", w.Name, string.Join(",", w.SubWords())));
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<Word> ReadFile(string filePath)
+        {
+            List<Word> words = new List<Word>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string word in lines.OrderBy(w => w).ToList())
+            {
+                if (!string.IsNullOrEmpty(word))
+                {
+                    words.Add(new Word(word));
+                }
+            }
+            return words;
+        }
+    }
+}
---
+++ TrieWordProcesser/Readme.txt
@@ -0,0 +1,23 @@
+﻿I have implemented the Trie datastructure to search a word from list of words. 
+We first read the file with list of words then sort it and insert the list to the Trie datastructure. The trie datastructure 
+will create a tree using each word in the list. 
+For each word in the list we are creating the Word object, The word object internally creates list of all possible words for 
+a word, For example for a word "catrat" the word object will split the word into below word:
+
+ca
+cat
+catr
+catra
+at
+atr
+atra
+atrat
+tr
+tra
+trat
+ra
+rat
+at
+
+Now each of the word from the above list will be search in Trie datastructure and added to the buffer. From the buffer list 
+we look for words that will make up the full word.
